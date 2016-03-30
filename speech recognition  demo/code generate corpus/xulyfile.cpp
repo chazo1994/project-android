aa127acfe5e438f108b7f4d336ba2aa2394f6bf0
chazo1994 <chazo1994@gmail.com>
2016-03-31 00:00:43 +0700

    commit data after train ngram, file format ngram arpa wirrten onto digitvocal.lm

--- speech recognition  demo/code generate corpus/xulyfile.cpp
+++ speech recognition  demo/code generate corpus/xulyfile.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+	int i=0;
 	fstream f1;
 	fstream f2;
 	f1.open("input.txt", ios::in);
@@ -15,8 +16,16 @@ int main(){
 		getline(f1, line);
 		//data += line;
 		line.erase(0,3);
-		line ="<s> moojt ngan" + line +"\n";
+		if(i<10) {
+			line =" khoong trawm linh" + line;// +"\n";
+		} if(i>=10 && i<100) {
+			line =" khoong trawm" + line + "\n";
+		} else {
+			line = line + "\n";
+		}
+		line ="<s> moojt nghifn" + line ;
 		f2<<line;
+		i++;
 	}
 	
 	f1.close();
